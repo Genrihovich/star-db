e02c2a0b81dbdf9247657f1be8a39c5a31134856
Slaventy1969 <slaventy.69@gmail.com>
2022-09-03 10:29:59 +0300

    Implementation ErrorIndicator

--- src/components/RandomPlanet/RandomPlanet.jsx
+++ src/components/RandomPlanet/RandomPlanet.jsx
@@ -3,6 +3,7 @@ import SwapiService from '../../services/swapiService';
 import './RandomPlanet.css';
 import { Spinner } from '../Spinner/Spinner';
 import PlanetView from './PlanetView';
+import ErrorIndicator from '../ErrorIndicator/ErrorIndicator';
 
 export default class RandomPlanet extends Component {
     swapiService = new SwapiService();
@@ -10,32 +11,46 @@ export default class RandomPlanet extends Component {
     state = {
         planet: {},
         loading: true,
-    }
+        error: false,
+    };
 
     constructor() {
         super();
         this.updatePlanet();
-    }
+    };
 
     onPlanetLoaded = (planet) => {
-        this.setState({ planet, loading: false });
-    }
+        this.setState({ planet, loading: false, error: false });
+    };
+
+    errors = () => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    };
 
     updatePlanet() {
         const id = (Math.floor(Math.random() * 25)) + 2;
         this.swapiService
             .getPlanet(id)
             .then(this.onPlanetLoaded)
+            .catch(this.errors);
     }
 
 
     render() {
-        const { planet, loading } = this.state;
+        const { planet, loading, error } = this.state;
+
+        const hasData = !(error || loading);//когда у нас нет ни загрузки ни ошибки
+
+        const errorMessage = error ? <ErrorIndicator /> : null;
         const spinner = loading ? <Spinner /> : null;
-        const planetView = !loading ? <PlanetView planet={planet} /> : null;
+        const planetView = hasData ? <PlanetView planet={planet} /> : null;
 
         return (
             <div className="random-planet jumbotron rounded">
+                {errorMessage}
                 {spinner}
                 {planetView}
             </div>
--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -3,8 +3,8 @@ import './Spinner.css';
 
 export const Spinner = () => {
     return (
-        <div class="loadingio-spinner-double-ring-nmlw8bfhjep">
-            <div class="ldio-ymd8zw7hv4a">
+        <div className="loadingio-spinner-double-ring-nmlw8bfhjep">
+            <div className="ldio-ymd8zw7hv4a">
                 <div></div>
                 <div></div>
                 <div>
